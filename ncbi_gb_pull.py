bbeba167567661dc805cf03bfc3985f4a7ff773f
Taylor Szasz Green <81335136+tszaszgreen@users.noreply.github.com>
2021-08-18 09:41:55 -0500

    Update ncbi_gb_pull.py

--- ncbi_gb_pull.py
+++ ncbi_gb_pull.py
@@ -9,16 +9,33 @@ species_list = ["Gallus gallus", "Numida meleagris"]
 #Must match name on NCBI taxonomy database.
 
 length = len(species_list)
-gID = "gene string"
-filename = "yourfile.txt"
-#Ex fox3
+f = open("geneName.txt", "w")
+filename = "geneName.txt"
+#Change geneName to your actual gene with each iteration of running this code
 
 for i in range(length):
   species = species_list[i]
-  handle = Entrez.esearch(db="nucleotide", term= species + "[Orgn] AND " + gID + "[Gene]", idtype="acc", retmax = 1)
+  handle = Entrez.esearch(db="nucleotide", term= species + "[Orgn] AND full title of gene from NCBI database", idtype="acc") 
+  #Change to fit each gene. Search your gene in the NCBI database and put the description after AND in the above line.
   record = Entrez.read(handle)
 
   idlist = record["IdList"]
-  handle = Entrez.efetch(db="nuccore", id=idlist, rettype="fasta_cds_na", retmax = 3, retmode = "text")
-  print(species, file =open(filename, "a"))
-  print(handle.read(),file =open(filename, "a"))
+  handle = Entrez.efetch(db="nuccore", id=idlist, rettype="fasta_cds_na", retmode = "text", retmax = 10)
+  h = handle.read()
+  great = h.count(">")
+
+  print(great)
+  if great > 1:
+    s = h.split(">")
+    for gene in s:
+      if "geneName" in gene:
+        #Remember to update this with your proper gene name
+        h = gene
+
+  if "geneName" in h:
+    #Update with correct gene name
+    print(species, file =open(filename, "a"))
+    print(h,file =open(filename, "a"))
+
+
+  time.sleep(2)
